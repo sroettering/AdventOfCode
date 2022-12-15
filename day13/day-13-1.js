860a7966f8560068ff828b95d90367e2e0800e85
Sven Röttering <sven.roettering@newcubator.com>
2022-12-15 16:33:09 +0100

    Day13

---
+++ day13/day-13-1.js
@@ -0,0 +1,74 @@
+import { readInput } from '../helpers/read-input.js';
+
+const pairs = readInput(new URL('day-13-1.txt', import.meta.url))
+    .split('\n')
+    .filter(line => line !== '')
+    .reduce((aggregate, line, idx) => {
+        if (idx % 2 === 0) {
+            aggregate.push([]);
+        }
+        aggregate[aggregate.length - 1].push(JSON.parse(line));
+        return aggregate;
+    }, []);
+
+const hasCorrectOrder = (itemA, itemB) => {
+    // console.log(itemA, ' - ', itemB);
+    for (let x = 0; x < Math.max(itemA.length, itemB.length); x++) {
+        const left = itemA[x];
+        const right = itemB[x];
+        if (!Array.isArray(left) && !Array.isArray(right)) {
+            // console.log('Comparing', itemA[x], itemB[x]);
+            if (left != null && right == null) {
+                return false;
+            }
+            if (left == null && right != null) {
+                return true;
+            }
+            if (left > right) {
+                return false;
+            }
+            if (left < right) {
+                return true;
+            }
+        } else if (Array.isArray(left) && Array.isArray(right)) {
+            if (!left.length && right.length) {
+                return true;
+            }
+            if (left.length && !right.length) {
+                return false;
+            }
+            if (!left.length && !right.length) {
+                return;
+            }
+            const checkOrder = hasCorrectOrder(left, right);
+            if (checkOrder === true) {
+                return true;
+            } else if (checkOrder === false) {
+                return false;
+            }
+        } else {
+            const currentA = Array.isArray(left) ? left : [left];
+            const currentB = Array.isArray(right) ? right : [right];
+            // console.log('comparing', itemA[x], currentA, itemB[x], currentB);
+            const checkOrder = hasCorrectOrder(currentA, currentB);
+            if (checkOrder === true) {
+                return true;
+            } else if (checkOrder === false) {
+                return false;
+            }
+        }
+    }
+}
+
+const pairToCompare = 1;
+// console.log(hasCorrectOrder(pairs[pairToCompare][0], pairs[pairToCompare][1]));
+
+pairs.forEach((pair, idx) => {
+    console.log(`Pair ${idx+1} has correct order:`, hasCorrectOrder(pair[0], pair[1]));
+});
+
+const result = pairs.reduce((sum, pair, idx) => {
+    const isCorrect = hasCorrectOrder(pair[0], pair[1]);
+    return sum + (isCorrect === true || isCorrect === undefined ? idx + 1 : 0);
+}, 0);
+console.log(result);
---
+++ day13/day-13-2.js
@@ -0,0 +1,80 @@
+import { readInput } from '../helpers/read-input.js';
+
+const packages = readInput(new URL('day-13-2.txt', import.meta.url))
+    .split('\n')
+    .filter(line => line !== '')
+    .map((line) => JSON.parse(line));
+
+packages.push([[2]]);
+packages.push([[6]]);
+
+const hasCorrectOrder = (itemA, itemB) => {
+    // console.log(itemA, ' - ', itemB);
+    for (let x = 0; x < Math.max(itemA.length, itemB.length); x++) {
+        const left = itemA[x];
+        const right = itemB[x];
+        if (!Array.isArray(left) && !Array.isArray(right)) {
+            // console.log('Comparing', itemA[x], itemB[x]);
+            if (left != null && right == null) {
+                return false;
+            }
+            if (left == null && right != null) {
+                return true;
+            }
+            if (left > right) {
+                return false;
+            }
+            if (left < right) {
+                return true;
+            }
+        } else if (Array.isArray(left) && Array.isArray(right)) {
+            if (!left.length && right.length) {
+                return true;
+            }
+            if (left.length && !right.length) {
+                return false;
+            }
+            if (!left.length && !right.length) {
+                return;
+            }
+            const checkOrder = hasCorrectOrder(left, right);
+            if (checkOrder === true) {
+                return true;
+            } else if (checkOrder === false) {
+                return false;
+            }
+        } else {
+            const currentA = Array.isArray(left) ? left : [left];
+            const currentB = Array.isArray(right) ? right : [right];
+            // console.log('comparing', itemA[x], currentA, itemB[x], currentB);
+            const checkOrder = hasCorrectOrder(currentA, currentB);
+            if (checkOrder === true) {
+                return true;
+            } else if (checkOrder === false) {
+                return false;
+            }
+        }
+    }
+}
+
+const sortedPackages = packages.sort((pairA, pairB) => {
+    const inOrder = hasCorrectOrder(pairA, pairB);
+    return inOrder === false ? 1 : -1;
+});
+
+console.log(sortedPackages);
+
+const dividerTwo = '[[2]]';
+const dividerSix = '[[6]]';
+const dividerIndices = [];
+
+for (let i = 0; i < sortedPackages.length; i++) {
+    if (JSON.stringify(sortedPackages[i]) === dividerTwo) {
+        dividerIndices.push(i+1);
+    }
+    if (JSON.stringify(sortedPackages[i]) === dividerSix) {
+        dividerIndices.push(i+1);
+    }
+}
+
+console.log(dividerIndices[0] * dividerIndices[1]);
