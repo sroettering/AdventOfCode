4b3115ea4b63aa25ab66281525b54b8c7711b17a
Sven Röttering <sven.roettering@newcubator.com>
2023-12-13 09:33:40 +0100

    Day12 2023

--- 2023/day12/day-12-1.js
+++ 2023/day12/day-12-1.js
@@ -17,96 +17,59 @@ const parseSprings = (lines) => {
 const springs = parseSprings(inputLines);
 
 const countArrangements = ({ springs, conditions }) => {
-    return checkArrangement(springs, conditions);
+    return countArrangement(springs, conditions);
 };
 
-const checkArrangement = (springs, conditions) => {
-    const groups = springs.split(/\.+/).filter((group) => group !== '');
-    console.log(springs, groups, conditions);
-
-    const sumSprings = groups.reduce((sum, group) => sum + group.length, 0);
-    const sumConditions = conditions.reduce((sum, condition) => sum + condition, 0);
-    if (sumSprings < sumConditions) {
+const countArrangement = (springs, conditions) => {
+    if (!conditions.length && !springs.includes('#')) {
+        // console.log('found arrangement');
+        return 1;
+    }
+    if (!conditions.length && springs.includes('#')) {
         return 0;
     }
 
-    const groupsWithSprings = groups.filter((group) => group.includes('#')).length;
-    if (groupsWithSprings > conditions.length) {
-        return 0;
-    }
+    const neededSprings = conditions.reduce((sum, condition) => sum + condition, 0) + conditions.length - 1;
 
-    if (groups[0].match(/^#+/)?.[0].length > conditions[0]) {
-        return 0;
-    }
-
-    if (groups[0].match(/^#+$/) && groups[0].length !== conditions[0]) {
-        return 0;
-    }
-
-    while (groups[0].match(new RegExp(`^#{${conditions[0]}}(?!#)`))) {
-        console.log('removing', conditions[0], 'from', groups[0]);
-        groups[0] = groups[0].substring(conditions[0]);
-        conditions.shift();
-        if (!groups[0].length) {
-            groups.shift();
-        } else if (!conditions.length && groups.every((group) => group.match(/^\?+$/))) { // consider removing else here
-            console.log('found permutation', groups, conditions);
-            return 1;
-        } else if (groups[0]?.startsWith('?')) {
-            // console.log('group continues with ?, setting it to .');
-            groups[0] = groups[0].replace('?', '.');
-            return checkArrangement(groups.join('.'), conditions);
+    let arrangements = 0;
+    for (let s = 0; s <= springs.length - neededSprings; s++) {
+        const group = springs.substring(s, s + conditions[0]);
+        // console.log('group', group, 'conditions', conditions, 'springs', springs, 'window at', s);
+        if (group.includes('.') && !group.startsWith('#')) {
+            // console.log('skipping group', group, conditions[0]);
+            continue;
+        } else if (group.includes('.')) {
+            return arrangements;
         }
 
-        if (groups.length === 0 && conditions.length === 0) {
-            console.log('found permutation', groups, conditions);
-            return 1;
-        } else if (groups.length === 0 || conditions.length === 0) {
-            // console.log('early exit', groups, conditions);
+        const nextChar = springs.charAt(s + conditions[0]);
+        if (nextChar === '#' && group.startsWith('#')) {
+            // console.log('arrangement not possible because of next char', nextChar);
+            return arrangements;
+        } else if (nextChar === '#') {
+            // console.log('group starts with ?, but is followed by #, skipping it');
+            continue;
+        }
+
+        const newSprings = springs.substring(s + conditions[0] + 1);
+        // console.log('group', group, 'is valid. rest springs', newSprings);
+
+        if (newSprings.length + 1 < neededSprings - conditions[0]) {
+            // console.log('not enough springs left', newSprings, neededSprings - conditions[0]);
             return 0;
         }
+
+        arrangements += countArrangement(newSprings, conditions.slice(1));
+
+        if (group.startsWith('#')) {
+            return arrangements;
+        }
     }
-
-    const broken = groups.slice();
-    broken[0] = broken[0].replace(/\?/, '.');
-    const working = groups.slice();
-    working[0] = working[0].replace(/\?/, '#');
-    // console.log('splitting', groups, 'into: ', broken, working);
-
-    return checkArrangement(working.join('.'), [...conditions]) + checkArrangement(broken.join('.'), [...conditions]);
+    return arrangements
 };
 
-// .?##
-// ..## , .###
-
-// console.log(countArrangements({ springs: '###', conditions: [3] }));
-// console.log(countArrangements({ springs: '???.##', conditions: [3, 2] }));
-// console.log(countArrangements({ springs: '???.###', conditions: [1, 1, 3] }));
-// console.log(countArrangements({ springs: '.??..??...?##.', conditions: [1, 1, 3] }));
-// console.log(countArrangements({ springs: '?##.', conditions: [3] }));
-// console.log(countArrangements({ springs: '?#?#?#?#?#?#?#?', conditions: [1, 3, 1, 6] }));
-// console.log(countArrangements({ springs: '?###????????', conditions: [3, 2, 1] }));
-// console.log(countArrangements({ springs: '???????????', conditions: [3, 2, 1] }));
-
-const row = springs[13];
-console.log(row, 'result: ', countArrangements({ springs: row.springs, conditions: [...row.conditions] }));
-
-// for (let r = 0; r < springs.length; r++) {
-//     console.log(`Row ${r}: `, countArrangements(springs[r]));
-// }
-
-// const result = springs.reduce((sum, row) => sum + countArrangements(row), 0);
-// console.log(result);
-
-// ??.##?#?##??.???
-// #..#######.#....
-// .#.#######.#....
-
-// #..#######...#..
-// .#.#######...#..
-
-// #..#######....#.
-// .#.#######....#.
-
-// #..#######.....#
-// .#.#######.....#
+const result = springs
+    .map((row, idx) => countArrangements(row))
+    .reduce((sum, arrangements) => sum + arrangements, 0);
+console.log(result);
+//7307
--- 2023/day12/day-12-2.js
+++ 2023/day12/day-12-2.js
@@ -1,5 +1,93 @@
 import { readInput } from '../../helpers/read-input.js';
 
-const inputLines = readInput(new URL('day-12-2-example.txt', import.meta.url))
+const inputLines = readInput(new URL('day-12-2.txt', import.meta.url))
     .split('\n')
     .filter(line => line !== '');
+
+const parseSprings = (lines) => {
+    return lines.map((line) => {
+        const [springs, conditions] = line.split(' ');
+        const expandedSprings = [springs, springs, springs, springs, springs].join('?');
+        const expandedConditions = [conditions, conditions, conditions, conditions, conditions].join(',');
+        return {
+            springs: expandedSprings,
+            conditions: expandedConditions.split(',').map((condition) => Number(condition)),
+        };
+    });
+};
+
+const springs = parseSprings(inputLines);
+
+const countArrangements = ({ springs, conditions }) => {
+    const cache = {};
+    return countArrangement(springs, conditions, cache);
+};
+
+const countArrangement = (springs, conditions, cache) => {
+    const cacheKey = `${springs}-${conditions.join(',')}`;
+    if (cache[cacheKey]) {
+        return cache[cacheKey];
+    }
+
+    if (!conditions.length && !springs.includes('#')) {
+        // console.log('found arrangement');
+        cache[cacheKey] = 1;
+        return 1;
+    }
+    if (!conditions.length && springs.includes('#')) {
+        cache[cacheKey] = 0;
+        return 0;
+    }
+
+    const neededSprings = conditions.reduce((sum, condition) => sum + condition, 0) + conditions.length - 1;
+
+    let arrangements = 0;
+    for (let s = 0; s <= springs.length - neededSprings; s++) {
+        const group = springs.substring(s, s + conditions[0]);
+        // console.log('group', group, 'conditions', conditions, 'springs', springs, 'window at', s);
+        if (group.includes('.') && !group.startsWith('#')) {
+            // console.log('skipping group', group, conditions[0]);
+            continue;
+        } else if (group.includes('.')) {
+            cache[cacheKey] = arrangements;
+            return arrangements;
+        }
+
+        const nextChar = springs.charAt(s + conditions[0]);
+        if (nextChar === '#' && group.startsWith('#')) {
+            // console.log('arrangement not possible because of next char', nextChar);
+            cache[cacheKey] = arrangements;
+            return arrangements;
+        } else if (nextChar === '#') {
+            // console.log('group starts with ?, but is followed by #, skipping it');
+            continue;
+        }
+
+        const newSprings = springs.substring(s + conditions[0] + 1);
+        // console.log('group', group, 'is valid. rest springs', newSprings);
+
+        if (newSprings.length + 1 < neededSprings - conditions[0]) {
+            // console.log('not enough springs left', newSprings, neededSprings - conditions[0]);
+            cache[cacheKey] = 0;
+            return 0;
+        }
+
+        arrangements += countArrangement(newSprings, conditions.slice(1), cache);
+
+        if (group.startsWith('#')) {
+            cache[cacheKey] = arrangements;
+            return arrangements;
+        }
+    }
+    cache[cacheKey] = arrangements;
+    return arrangements
+};
+
+const result = springs
+    .map((row, idx) => {
+        console.log('analyzing row', idx);
+        return countArrangements(row);
+    })
+    .reduce((sum, arrangements) => sum + arrangements, 0);
+console.log(result);
+//3415570893842
